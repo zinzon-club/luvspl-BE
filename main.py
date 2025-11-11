3c69fee9dee6c6a97db00b8a0830167ba7ba7944
dpqlsn <a01037111209@gmail.com>
2025-11-11 10:12:07 +0900

    hotfix :: CORS 추가

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://luvspl-fe.vercel.app",
+        "https://luvspl.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
