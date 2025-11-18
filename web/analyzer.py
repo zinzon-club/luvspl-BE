73fd9a457f6566ed250acda3c33125bfa7b282da
soeun823 <bsoeun866@gmail.com>
2025-11-18 11:08:10 +0900

    refactor(#3) :: userid 저장하는 로직 변경

--- web/analyzer.py
+++ web/analyzer.py
@@ -1,26 +1,28 @@
-from fastapi import APIRouter, UploadFile, Form, Depends,Request, Security
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import APIRouter, UploadFile, Form
 import pandas as pd
 from transformers import pipeline
 from model.sentiment_model import load_sentiment_model
 from service.analysis import analyze_conversation
 from data.analyze import save_analysis
-from utils.security import get_current_user_id
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/kakao/login")
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/kakao/token-login")
 router = APIRouter()
 
 print("Loading models...")
 sentiment_model = load_sentiment_model()
-classifier_model = pipeline("zero-shot-classification", model="joeddav/xlm-roberta-large-xnli")
+classifier_model = pipeline(
+    "zero-shot-classification", model="joeddav/xlm-roberta-large-xnli"
+)
 print("Models loaded successfully.")
 
+
 @router.post("/analyze")
-async def analyze_file(request: Request, file: UploadFile, username: str = Form(...), user_id: int = Depends(get_current_user_id), token: str = Security(oauth2_scheme)):
-    # 헤더 로그 출력
-    print("=== REQUEST HEADERS ===")
-    print(request.headers)
-    print("=======================")
+async def analyze_file(
+    file: UploadFile,
+    user_id: int,
+    username: str = Form(...),
+):
+
     df = pd.read_csv(file.file)
     result = analyze_conversation(df, username, sentiment_model, classifier_model)
     save_analysis(user_id, result)
