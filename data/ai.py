4b71ee32820b11cdd20e7ec21f4a328e3aa36b30
dpqlsn <a01037111209@gmail.com>
2025-12-08 08:25:07 +0900

    feat :: 챗봇

---
+++ data/ai.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter
+from dotenv import load_dotenv
+from model.schemas import KakaoRequest
+from service.ai import analyze_sentence
+
+router = APIRouter()
+
+@router.post("/bot")
+async def bot(req: KakaoRequest):
+    # userRequest 유효성 검사
+    if not req.userRequest or not req.userRequest.get("utterance"):
+        return {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {"simpleText": {"text": "⚠️ 문장을 다시 입력해줘!"}}
+                ]
+            }
+        }
+
+    utterance = req.userRequest["utterance"]
+
+    answer = await analyze_sentence(utterance)
+
+    return {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {"simpleText": {"text": answer}}
+            ]
+        }
+    }
